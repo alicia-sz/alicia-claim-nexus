dc990ab1f40e51527e8a34344ae3cc0278d30d26
Alicia Sanchez <alicia.d.sanchez@outlook.com>
2025-05-04 22:45:31 -0400

    google sign-in added

--- app/page.tsx
+++ app/page.tsx
@@ -15,24 +15,6 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
-
-  useEffect(() => {
-    listTodos();
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
-
   return (
     <Authenticator>
       {({ signOut, user }) => (
